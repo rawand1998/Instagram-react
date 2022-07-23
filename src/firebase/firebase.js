aa8044b5097dbdbab96ae042cdf07e386d9958af
rawand1998 <rawandgaradh@gmail.com>
2022-07-23 17:49:28 +0300

    firebase install

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyAFX9Gydx2AqNMMtAYgA29O3RaeU9lt3D8",
+  authDomain: "insta-f173c.firebaseapp.com",
+  projectId: "insta-f173c",
+  storageBucket: "insta-f173c.appspot.com",
+  messagingSenderId: "924806025855",
+  appId: "1:924806025855:web:381dab776e7e14adae254a",
+  measurementId: "G-231PB7TZSC",
+});
+const db = firebaseConfig.firestore();
+const auth = firebase.auth();
+export { db, auth };
