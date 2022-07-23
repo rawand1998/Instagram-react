238086545adb336dc241567751f83aa2d4cd86cd
rawand1998 <rawandgaradh@gmail.com>
2022-07-23 18:49:25 +0300

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home'
 function App() {
   return (
     <div className="App">
-    
+    <Header />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        
+      </Routes>
+    </Router>
     </div>
   );
 }
