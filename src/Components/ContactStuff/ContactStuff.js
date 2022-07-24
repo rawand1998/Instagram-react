a7f896ac883049a74fcfa0ab8d0cd5f1a0183c54
rawand1998 <rawandgaradh@gmail.com>
2022-07-24 09:52:11 +0300

    logout

--- src/Components/ContactStuff/ContactStuff.js
+++ src/Components/ContactStuff/ContactStuff.js
@@ -1,11 +1,24 @@
 import Avatar from "@mui/material/Avatar";
 import React from 'react'
 import './Style.css'
-import {useSelector} from 'react-redux'
-import {selectName,selectPhoto} from '../../features/User/UserSlice'
+import {useSelector,useDispatch} from 'react-redux'
+import {auth} from '../../firebase/firebase'
+import {selectName,selectPhoto,setLogOut} from '../../features/User/UserSlice'
+
 function ContactStuff() {
   const name = useSelector(selectName)
   const photo = useSelector(selectPhoto)
+  const dispatch = useDispatch()
+  const logout = ()=>{
+    auth.signOut().then((res)=>{
+      dispatch(setLogOut({
+        name:null,
+        email:null,
+        photo:null,
+        uid:null,
+      }))
+    })
+  }
   return (
     <div className="contact-conatiner">
       <div className="contact-warrp">
@@ -14,7 +27,7 @@ function ContactStuff() {
           <Avatar src={photo} />
           <span>welcome  {name}</span>
         </div>
-        <div>Logout</div>
+        <div onClick={logout}>Logout</div>
       </div>
     </div>
   )
